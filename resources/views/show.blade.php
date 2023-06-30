a98cb5f7b72c1bb242cde17a07cbe638fcc7a98f
DanieleSavoia <danielesavoia44@gmail.com>
2023-06-30 17:58:18 +0200

    with-card-details

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,13 @@
-<h1 class="text-center danger">{{$comics->title}}</h1>
-<img src="{{ $comics->thumb }}" class="" alt="...">
-<p class="">{{$comics->description}}</p>
-<a href="#" class="btn btn-primary">{{$comics->price}}</a>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1 class="text-center danger mb-4">{{$comics->title}}</h1>
+    <img src="{{ $comics->thumb }}" class="mb-4 rounded mx-auto d-block" alt="...">
+    <p class="mb-3">{{$comics->description}}</p>
+    <p class="text-center"><a class="btn btn-primary btn-lg" href="#" role="button">{{$comics->price}}</a></p>
+</body>
